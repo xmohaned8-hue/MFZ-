502dba9176e65d42366bb6e4ecbf755c39f33481
xmohaned8-hue <xmohaned8@gmail.com>
2026-02-12 20:22:18 +0100

    Update and rename Quantum.lua to ZXCRATMZF.lua

--- ZXCRATMZF.lua
+++ ZXCRATMZF.lua
@@ -1,10 +1,11 @@
 -- [[ MZF OFFICIAL UNIVERSAL SCRIPT ]] --
--- [[ OWNER: xmohaned8-hue ]] --
+-- [[ SYSTEM: ZXCRATMZF OMNI ]] --
 
 local LP = game.Players.LocalPlayer
 local RS = game:GetService("RunService")
 
--- محرك السرعة واختراق الجدران
+print("🌑 ZXCRATMZF SYSTEM: ONLINE ✅")
+
 RS.RenderStepped:Connect(function()
     if LP.Character and LP.Character:FindFirstChild("HumanoidRootPart") then
         local HRP = LP.Character.HumanoidRootPart
@@ -12,19 +13,17 @@ RS.RenderStepped:Connect(function()
         if Hum.MoveDirection.Magnitude > 0 then
             HRP.CFrame = HRP.CFrame + (Hum.MoveDirection * 2.5)
         end
-        -- خاصية الـ Noclip
         for _, v in pairs(LP.Character:GetDescendants()) do
             if v:IsA("BasePart") then v.CanCollide = false end
         end
     end
 end)
 
--- تنوير الماب وإزالة اللاق
 game.Lighting.Brightness = 2
 game.Lighting.FogEnd = 1e5
-for _,v in pairs(game:GetDescendants()) do
-    if v:IsA("BasePart") then v.Material = "SmoothPlastic" end
+for _, v in pairs(game:GetDescendants()) do
+    if v:IsA("BasePart") then 
+        v.Material = Enum.Material.SmoothPlastic 
+        v.CastShadow = false
+    end
 end
-
-print("🌑 MZF QUANTUM ACTIVE ✅")
- 
